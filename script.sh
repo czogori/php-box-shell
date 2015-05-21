248dbf0c16da1a0e21a564a10e55abe897929794
Arek Jaskólski <arek.jaskolski@gmail.com>
2015-05-21 23:01:29 +0200

    Added oh my zsh

--- script.sh
+++ script.sh
@@ -3,9 +3,10 @@ sudo apt-get update
 sudo apt-get install -y \
 git mc \
 php5 php5-pgsql php5-curl php5-intl \
-postgresql postgresql-contrib
+postgresql postgresql-contrib \
+zsh
 
-
+# postgres
 sudo -u postgres psql -c "alter user postgres password 'postgres';"
 sudo -u postgres psql -c "CREATE ROLE aj WITH LOGIN PASSWORD 'aj' CREATEDB CREATEROLE;"
 sudo -u postgres psql -c "create database aj with owner aj;"
@@ -15,10 +16,11 @@ sudo sed -i "s/peer/md5/" /etc/postgresql/9.3/main/pg_hba.conf
 sudo sed -i "s/127.0.0.1\/32/0.0.0.0\/0/" /etc/postgresql/9.3/main/pg_hba.conf
 sudo /etc/init.d/postgresql restart
 
+# composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-
+# apache
 sudo sed -i "s/;date.timezone =/date.timezone = Europe\/Warsaw/" /etc/php5/apache2/php.ini
 sudo sed -i "s/;date.timezone =/date.timezone = Europe\/Warsaw/" /etc/php5/cli/php.ini
 sudo sed -i "s/\/var\/www\/html/\/var\/www\/app\/web/" /etc/apache2/sites-enabled/000-default.conf
@@ -28,3 +30,8 @@ sudo apache2ctl graceful
 # install phpunit
 wget https://phar.phpunit.de/phpunit.phar
 sudo mv phpunit.phar /usr/local/bin/phpunit && sudo chmod +x /usr/local/bin/phpunit
+
+# oh my zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+sudo chsh -s $(which zsh) vagrant
+wget https://raw.githubusercontent.com/czogori/dotfiles/master/.zshrc -O ~/.zshrc
