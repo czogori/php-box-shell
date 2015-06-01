8a37c5da3590c4c3adfa8c552b7b4f0ca9e91cfe
Arkadiusz Jaskólski <arek.jaskolski@gmail.com>
2015-06-01 17:27:19 +0200

    Removed apache

--- script.sh
+++ script.sh
@@ -1,12 +1,6 @@
 sudo apt-get install -y \
 php5 php5-pgsql php5-curl php5-intl
 
-# apache
-sudo sed -i "s/;date.timezone =/date.timezone = Europe\/Warsaw/" /etc/php5/apache2/php.ini
-sudo sed -i "s/;date.timezone =/date.timezone = Europe\/Warsaw/" /etc/php5/cli/php.ini
-sudo sed -i "s/\/var\/www\/html/\/var\/www\/app\/web/" /etc/apache2/sites-enabled/000-default.conf
-sudo apache2ctl graceful
-
 # composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
